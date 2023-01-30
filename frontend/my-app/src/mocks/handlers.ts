3aa95612f24bf5ab48b996691570b8ee6fdc3e17
zshafe <zshafe@cs.washington.edu>
2023-01-30 15:15:49 -0800

    other init linter changes

--- frontend/my-app/src/mocks/handlers.ts
+++ frontend/my-app/src/mocks/handlers.ts
@@ -1,118 +1,117 @@
 import { rest } from 'msw'
 import type { RestHandler } from 'msw'
 
-export const handlers : RestHandler[] = [
+export const handlers: RestHandler[] = [
 
-    rest.get('/locations', (req, res, ctx) => {
-      const users = req.url.searchParams.getAll('users')
-      // TODO - remove console.log
-      console.log(users)
+  rest.get('/locations', (req, res, ctx) => {
+    const users = req.url.searchParams.getAll('users')
+    // TODO - remove console.log
+    console.log(users)
 
+    return res(
+      ctx.status(200),
+      ctx.json({
+        locations: [
+          {
+            id: 1,
+            name: 'CSE1',
+            rating: '4 stars',
+            address: 'Paul Allen Center, 185 E Stevens Way NE AE100R, Seattle, WA 98195',
+            linkToDirections: 'www.directions.com',
+            travelTimes: [12, 20, 8, 15, 16]
+          },
+          {
+            id: 2,
+            name: 'CSE2',
+            rating: '5 stars',
+            address: 'Bill & Melinda Gates Center For Computer Science & Engineering, 3800 E Stevens Way NE, Seattle, WA 98195',
+            linkToDirections: 'www.directions.com',
+            travelTimes: [12, 20, 8, 15, 16]
+          }
+        ]
+      })
+    )
+  }),
+
+  rest.get('/user', (req, res, ctx) => {
+    const email = req.url.searchParams.get('email')
+    const password = req.url.searchParams.get('password')
+    // TODO - remove console.log
+    console.log(email)
+    console.log(password)
+
+    if (password === 'password') {
       return res(
         ctx.status(200),
         ctx.json({
-          locations: [
+          listOfFriends: [
             {
               id: 1,
-              name: 'CSE1',
-              rating: '4 stars',
-              address: 'Paul Allen Center, 185 E Stevens Way NE AE100R, Seattle, WA 98195',
-              linkToDirections: 'www.directions.com',
-              travelTimes: [12, 20, 8, 15, 16]
+              name: 'Joe',
+              address: 'Paul Allen Center, 185 E Stevens Way NE AE100R, Seattle, WA 98195'
             },
             {
               id: 2,
-              name: 'CSE2',
-              rating: '5 stars',
-              address: 'Bill & Melinda Gates Center For Computer Science & Engineering, 3800 E Stevens Way NE, Seattle, WA 98195',
-              linkToDirections: 'www.directions.com',
-              travelTimes: [12, 20, 8, 15, 16]
+              name: 'Nelson',
+              address: 'Bill & Melinda Gates Center For Computer Science & Engineering, 3800 E Stevens Way NE, Seattle, WA 98195'
             }
           ],
-        }),
-      )
-    }),
+          listOfRequests: [
+            {
 
-    rest.get('/user', (req, res, ctx) => {
-      const email = req.url.searchParams.get('email')
-      const password = req.url.searchParams.get('password')
-      // TODO - remove console.log
-      console.log(email)
-      console.log(password)
-
-      if (password === 'password') {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            listOfFriends: [
-              {
-                id: 1,
-                name: 'Joe',
-                address: 'Paul Allen Center, 185 E Stevens Way NE AE100R, Seattle, WA 98195'
-              },
-              {
-                id: 2,
-                name: 'Nelson',
-                address: 'Bill & Melinda Gates Center For Computer Science & Engineering, 3800 E Stevens Way NE, Seattle, WA 98195'
-              }
-            ],
-            listOfRequests: [
-              {
-
-              }
-            ]
-          }),
-        )
-      }
-
-      return res(
-        ctx.status(401),
-        ctx.json({
-          errorMessage: 'Invalid login credentials'
+            }
+          ]
         })
       )
+    }
 
-    }),
+    return res(
+      ctx.status(401),
+      ctx.json({
+        errorMessage: 'Invalid login credentials'
+      })
+    )
+  }),
 
-    rest.get('/friends', (req, res, ctx) => {
-      const email = req.url.searchParams.get('email')
-      // TODO - remove console.log
-      console.log(email)
+  rest.get('/friends', (req, res, ctx) => {
+    const email = req.url.searchParams.get('email')
+    // TODO - remove console.log
+    console.log(email)
 
-      // TODO - right now this if there is any header called 'auth' the request will go through
-      const auth = req.headers.has('auth')
+    // TODO - right now this if there is any header called 'auth' the request will go through
+    const auth = req.headers.has('auth')
 
-      if (auth) {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            listOfFriends: [
-              {
-                id: 1,
-                name: 'Joe',
-                address: 'Paul Allen Center, 185 E Stevens Way NE AE100R, Seattle, WA 98195'
-              },
-              {
-                id: 2,
-                name: 'Nelson',
-                address: 'Bill & Melinda Gates Center For Computer Science & Engineering, 3800 E Stevens Way NE, Seattle, WA 98195'
-              }
-            ],
-            listOfRequests: [
-              {
-
-              }
-            ]
-          }),
-        )
-      }
-  
+    if (auth) {
       return res(
-        ctx.status(401),
+        ctx.status(200),
         ctx.json({
-          errorMessage: 'Error: Not Authenticated'
-        }),
+          listOfFriends: [
+            {
+              id: 1,
+              name: 'Joe',
+              address: 'Paul Allen Center, 185 E Stevens Way NE AE100R, Seattle, WA 98195'
+            },
+            {
+              id: 2,
+              name: 'Nelson',
+              address: 'Bill & Melinda Gates Center For Computer Science & Engineering, 3800 E Stevens Way NE, Seattle, WA 98195'
+            }
+          ],
+          listOfRequests: [
+            {
+
+            }
+          ]
+        })
       )
-    }),
+    }
+
+    return res(
+      ctx.status(401),
+      ctx.json({
+        errorMessage: 'Error: Not Authenticated'
+      })
+    )
+  })
 
 ]
