3bc212bb3840c9aad666c3d4ef4034a3b2c1e470
jspaniac <jspaniac@cs.washington.edu>
2023-02-06 14:42:43 -0800

    Updating to use fetch and work with mocks

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import type { EquidistantRequest } from './requestObjects'
+import { LoginRequest } from './requestObjects'
+import type { LoginResponse } from './responseTypes'
+
+async function makeRequest (request: EquidistantRequest): Promise<LoginResponse> {
+  const response = await fetch(request.path, request)
+  const json = await response.json()
+  return json
+}
 
 function App (): React.ReactElement {
+  useEffect(() => {
+    makeRequest(new LoginRequest('user', 'password'))
+      .then(res => {
+        console.log(res.listOfFriends)
+      })
+      .catch((e) => { console.error(e) })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
