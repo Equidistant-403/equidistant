468bf066dd67ffbcafc5d6110e6d13373114bb36
Nelson Sun <31164153+pandabear15@users.noreply.github.com>
2023-02-07 14:22:43 -0800

    Backend CI (#11)
    * Transitioned to pytest instead of unittest, and added a workflow for backend unittesting
    
    * extra dash in workflow
    
    * tests do not need to pass self

--- backend/tests/basic_test.py
+++ backend/tests/basic_test.py
@@ -1,10 +1,2 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_something():
+    assert (True and True) == (True or True)
