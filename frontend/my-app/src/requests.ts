cdd42f67da8835436ea4b97b8af23d20995f259a
jspaniac <jspaniac@cs.washington.edu>
2023-02-09 15:10:26 -0800

    Moved makeRequest export

--- frontend/my-app/src/requests.ts
+++ frontend/my-app/src/requests.ts
@@ -1,9 +1,10 @@
 import type { EquidistantRequest } from './requestObjects'
 import type { EquidistantResponse } from './responseTypes'
-export { makeRequest }
 
 async function makeRequest (request: EquidistantRequest): Promise<EquidistantResponse> {
   const response = await fetch(request.path, request)
   const json = await response.json()
   return json
 }
+
+export { makeRequest }
