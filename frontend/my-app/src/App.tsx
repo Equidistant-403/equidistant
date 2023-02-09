a0782ea954c6dc7942e321487350b8771ab73848
Joe Spaniac <36169310+jspaniac@users.noreply.github.com>
2023-02-09 14:15:29 -0800

    Refactoring responses & deleting junk from app.tsx (#17)
    * Refactoring responses & deleting junk from app.tsx
    
    * Fixing code to pass CI
    
    ---------
    
    Co-authored-by: jspaniac <jspaniac@cs.washington.edu>

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,28 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './App.css'
-import type { EquidistantRequest } from './requestObjects'
-import { LoginRequest } from './requestObjects'
-import type { LoginResponse } from './responseTypes'
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './Pages/LoginPage'
 import LandingPage from './Pages/LandingPage'
 import Results from './Pages/Results'
 import Account from './Pages/Account'
 
-async function makeRequest (request: EquidistantRequest): Promise<LoginResponse> {
-  const response = await fetch(request.path, request)
-  const json = await response.json()
-  return json
-}
-
 function App (): React.ReactElement {
-  useEffect(() => {
-    makeRequest(new LoginRequest('user', 'password'))
-      .then(res => {
-        console.log(res.listOfFriends)
-      })
-      .catch((e) => { console.error(e) })
-  }, [])
   return (
     <Routes>
       <Route path="/" element={<LoginPage/>} />
