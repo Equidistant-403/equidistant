e3d555667647a695cc03e0c3b7e285acfac01005
pandabear100 <nelsonysun@gmail.com>
2023-02-09 22:24:28 -0800

    Further rearrangement of files and addition of BitdotioDB implementation

--- backend/src/database/Database.py
+++ backend/src/database/Database.py
@@ -9,23 +9,22 @@ class Database(ABC):
     """
 
     @abstractmethod
-    def read(self, query: str) -> array:
+    def __init__(self, token: str):
         """
-        Performs a read operation on the database, returning all
-        data matching the query.
-        :param query: the read query sent to the database
-        :return: an array (possibly multidimensional) of all data that
-        matches the passed query
+        Initializes the Database object. Requires an API token.
+        :param token: the API token of the database
         """
         pass
 
     @abstractmethod
-    def write(self, query: str) -> array:
+    def db_query(self, query: str) -> array:
         """
-        Performs a write operation on the database, optionally returning
-        data as specified by the query.
-        :param query: the write query sent to the database
-        :return: an array, if nothing is returned by the database then
-        an empty array is returned.
+        Queries the database, returning all data matching the query.
+        :param query: the query sent to the database in SQL-like language
+        :return: an array of tuples of all data that matches the passed query.
+        If nothing is returned by the database, then this method returns an empty
+        array.
+        :raises: ValueError if query is malformed
+                 ConnectionError if unable to establish connection with the database
         """
         pass
