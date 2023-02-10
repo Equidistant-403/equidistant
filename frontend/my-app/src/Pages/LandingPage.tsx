5716842685db68603cd3e994ef11d11014d5003d
jspaniac <jspaniac@cs.washington.edu>
2023-02-10 11:57:18 -0800

    Adding result generation query and connecting to account and results page

--- frontend/my-app/src/Pages/LandingPage.tsx
+++ frontend/my-app/src/Pages/LandingPage.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import './LandingPage.css'
 import { useLocation, useNavigate } from 'react-router-dom'
-import type { User } from '../responseTypes'
+import { isError } from '../responseTypes'
+import type { LocationResponse, User } from '../responseTypes'
+import { makeRequest } from '../requests'
+import { LocationRequest } from '../requestObjects'
 
 const LandingPage: React.FC = () => {
   const location = useLocation()
-
   const navigate = useNavigate()
+
   const [friends, setFriends] = useState<User[]>(location.state.friends)
+  // TODO: Allow user to accept friend requests
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [requests, setRequests] = useState<User[]>(location.state.requests)
+  const user = location.state.user
+  const bearer = location.state.bearer
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [checkedFriends, setCheckedFriends] = useState<string[]>([]) // holds checked friends list
@@ -30,12 +38,32 @@ const LandingPage: React.FC = () => {
   }
 
   const handleGenerateClick = (): void => {
-    console.log('dummy generation actuation')
-    navigate('/results')
+    makeRequest(new LocationRequest([user.email, ...checkedFriends], bearer))
+      .then((res) => {
+        if (isError(res)) {
+          // TODO: Display this error message
+          // TODO: remove console.log
+          console.log(res.errorMessage)
+          return
+        }
+
+        const response = (res as LocationResponse)
+        navigate('/results', {
+          state: {
+            locations: response.locations
+          }
+        })
+      })
+      .catch((e) => { console.error(e) })
   }
 
   const handlAccountClick = (): void => {
-    navigate('/account')
+    navigate('/account', {
+      state: {
+        email: user.email,
+        location: user.address
+      }
+    })
   }
 
   const handleLogoutClick = (): void => {
@@ -45,6 +73,7 @@ const LandingPage: React.FC = () => {
 
   return (
     <div className="container">
+      <p>Current user: {user.email}</p>
       <div className="buttons">
         <button onClick={handleGenerateClick}>Generate</button>
         <button onClick={handlAccountClick}>Account</button>
