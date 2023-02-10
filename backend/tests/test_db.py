de872c0ed38ecc7f06d96cd65f5651c076325471
pandabear100 <nelsonysun@gmail.com>
2023-02-09 21:47:14 -0800

    Added environmental variable finding of the API Token so it does not live in the repository anymore (security issue; API token has been changed), and commented test cases.

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -3,12 +3,29 @@ from backend.src.database.BitdotioDB import BitdotioDB
 import os
 
 
+def test_retrieve_db_token():
+    """
+    This test verifies that the BITIO_API_TOKEN environmental variable
+    is set. Without such an environmental variable, connection with the
+    database cannot proceed.
+    """
+    assert os.getenv('BITIO_API_TOKEN') is not None
+
+
 def test_access_table():
+    """
+    This test simply checks if a connection to the bit.io database can
+    be established. If this test errors, then connection has failed.
+    """
     db = BitdotioDB(token=os.getenv('BITIO_API_TOKEN'))
-    db.db_query('SELECT')  # If this statement errors, then connection has failed
+    db.db_query('SELECT')
 
 
 def test_read_write_from_table():
+    """
+    This test verifies that it is possible to both read and write to the
+    bit.io database.
+    """
     db = BitdotioDB(token=os.getenv('BITIO_API_TOKEN'))
     tuples = db.db_query(f'DELETE FROM {Constants.DB_TEST}')  # delete all data from the test table
     assert len(tuples) == 0
@@ -20,6 +37,10 @@ def test_read_write_from_table():
 
 
 def test_read_no_entries_selected():
+    """
+    This test ensures correct edge behavior should a read query return
+    no matching rows: that is, an empty list.
+    """
     db = BitdotioDB(token=os.getenv('BITIO_API_TOKEN'))
     tuples = db.db_query(f'DELETE FROM {Constants.DB_TEST}')  # delete all data from the test table
     assert len(tuples) == 0
