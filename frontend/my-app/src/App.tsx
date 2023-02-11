bfd1e15f4dc2b849d271913dad51a59193b84177
zshafe <zshafe@cs.washington.edu>
2023-02-10 17:41:08 -0800

    remove unused css

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './Pages/LoginPage'
 import LandingPage from './Pages/LandingPage'
