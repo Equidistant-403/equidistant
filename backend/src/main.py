a6046d6dfaa5bf33a4641364859c64f49739e291
pandabear100 <nelsonysun@gmail.com>
2023-02-13 13:19:41 -0800

    Added baseline logic for main.py

---
+++ backend/src/main.py
@@ -0,0 +1,22 @@
+def login_request(email: str, password: str) -> bool:
+    pass
+
+
+def get_friend_list(email: str, bearer: str | None):
+    pass
+
+
+def get_self_profile(email: str, bearer: str | None):
+    pass
+
+
+def create_user(email: str, password: str, address: str):
+    pass
+
+
+def send_friend_req(requester: str, receiver: str):
+    pass
+
+
+def respond_friend_req(requester: str, receiver: str, response: bool):
+    pass
