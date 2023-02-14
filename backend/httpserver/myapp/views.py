483c85a9a0045bb97a4aaf557e3bde93a57cd1e9
aarushidutta <a23dutta@uw.edu>
2023-02-14 14:16:13 -0800

    httpserver partially implemented

---
+++ backend/httpserver/myapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
+from django.http import HttpResponse
+
+def post_create_user(request):
+    if request.method == 'POST':
+        data = request.POST
+    return HttpResponse('Thanks for submitting your data')
+
+def get_login_credentials(request):
+    if request.method == 'GET':
+        data = request.GET
+    return HttpResponse('Thanks for submitting your data')
+
+def get_locations(request):
+    if request.method == 'GET':
+        data = request.GET
+    return HttpResponse('Thanks for submitting your data')
+
+def get_friends(request):
+    if request.method == 'GET':
+        data = request.GET
+    return HttpResponse('Thanks for submitting your data')
+
+def get_user(request):
+    if request.method == 'GET':
+        data = request.GET
+    return HttpResponse('Thanks for submitting your data')
+
+def post_friend_request(request):
+    if request.method == 'POST':
+        data = request.POST
+    return HttpResponse('Thanks for submitting your data')
+
+def post_friend_request_response(request):
+    if request.method == 'POST':
+        data = request.POST
+    return HttpResponse('Thanks for submitting your data')
