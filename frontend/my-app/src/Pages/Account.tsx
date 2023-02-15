f74ab0195eae1912f84a40010cc3d37c63994263
zshafe <zshafe@cs.washington.edu>
2023-02-15 15:15:40 -0800

    initial work - needs styling and header

--- frontend/my-app/src/Pages/Account.tsx
+++ frontend/my-app/src/Pages/Account.tsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react'
 import './Account.css'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { Box, Button, Grid, TextField, Typography } from '@mui/material'
 
 const Account: React.FC = () => {
+  const NEW_EMAIL = 'New Email'
+  const NEW_ADDRESS = 'New Address'
+  const NEW_PASSWORD = 'New Password'
+  const NEW_CONFIRM_PASSWORD = 'Confirm New Password'
+
   const location = useLocation()
   const navigate = useNavigate()
 
   const [email, setEmail] = useState<string>(location.state.email)
-  const [userLocation, setLocation] = useState<string>(location.state.location)
-  const [password, setPassword] = useState<string>('')
-  const [passwordConfirm, setPasswordConfirm] = useState<string>('')
+  const [newEmail, setNewEmail] = useState<string>(NEW_EMAIL)
+  const [address, setAddress] = useState<string>(location.state.location)
+  const [newAddress, setNewAddress] = useState<string>(NEW_ADDRESS)
+  const [newPassword, setNewPassword] = useState<string>(NEW_PASSWORD)
+  const [confirmPassword, setConfirmPassword] = useState<string>(NEW_CONFIRM_PASSWORD)
 
   const handleDeleteAccount = (): void => {
-    console.log('Delete Account dummy function')
+    console.log('Delete Account dummy function (STRETCH GOAL)')
   }
 
   const handleUploadImage = (): void => {
-    console.log('Upload Image dummy function')
+    console.log('Upload Image dummy function (STRETCH GOAL)')
   }
 
   const handleBack = (): void => {
@@ -24,74 +32,79 @@ const Account: React.FC = () => {
   }
 
   const handleUpdateEmail = (): void => {
-    console.log('Update Email dummy function')
+    setEmail(newEmail)
+    setNewEmail(NEW_EMAIL)
+    console.log('Update Email dummy function (STRETCH GOAL)')
   }
 
   const handleUpdateLocation = (): void => {
-    console.log('Update Location dummy function')
+    setAddress(newAddress)
+    setNewAddress(NEW_ADDRESS)
+    console.log('Update Location dummy function (STRETCH GOAL)')
   }
 
   const handleUpdatePassword = (): void => {
-    console.log('Update Password dummy function')
+    console.log('Update Password dummy function (STRETCH GOAL)')
   }
 
   return (
-        <div className="container">
-        <div className="left-column">
-            <button className="upload-image" onClick={handleUploadImage}>
-                Upload Image
-            </button>
-            <button className="delete-account" onClick={handleDeleteAccount}>
-                Delete Account
-            </button>
-            <button className="back" onClick={handleBack}>
-                Back
-            </button>
-        </div>
-        <div className="right-column">
-            <div className="title">Username</div>
-            <div className="email-address-title">Email Address</div>
-            <input
-                className="email-address"
-                type="email"
-                value={email}
-                onChange={e => { setEmail(e.target.value) }}
-                placeholder="Email"
+    <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+      <Grid
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid xs={6}>
+            <Typography
+              variant="h5"
+              component="h2">
+                Email: {email}
+            </Typography>
+            <TextField
+              defaultValue={newEmail}
+              onChange={e => { setNewEmail(e.target.value) }}
             />
-            <button className="update-email" onClick={handleUpdateEmail}>
-            Update
-            </button>
-            <div className="location-title">Location</div>
-            <input
-                className="location"
-                type="location"
-                value={userLocation}
-                onChange={e => { setLocation(e.target.value) }}
-                placeholder="Location"
+            <Button color='secondary' variant='contained' onClick={handleUpdateEmail}>Update Email</Button>
+          </Grid>
+          <Grid xs={6}>
+            <Typography
+              variant="h5"
+              component="h2">
+                Address: {address}
+            </Typography>
+            <TextField
+              defaultValue={newAddress}
+              onChange={e => { setNewAddress(e.target.value) }}
             />
-            <button className="update-location" onClick={handleUpdateLocation}>
-            Update
-            </button>
-            <div className="password-title">Update Password</div>
-            <input
-                type="password"
-                className="password"
-                value={password}
-                onChange={e => { setPassword(e.target.value) }}
-                placeholder="Password"
+            <Button color='secondary' variant='contained' onClick={handleUpdateLocation}>Update Email</Button>
+          </Grid>
+          <Grid xs={6}>
+            <Typography
+              variant="h5"
+              component="h2">
+                Password
+            </Typography>
+            <TextField
+              defaultValue={newPassword}
+              onChange={e => { setNewPassword(e.target.value) }}
             />
-            <input
-                type="password"
-                className="password-confirm"
-                value={passwordConfirm}
-                onChange={e => { setPasswordConfirm(e.target.value) }}
-                placeholder="Confirm Password"
+            <TextField
+              defaultValue={confirmPassword}
+              onChange={e => { setConfirmPassword(e.target.value) }}
             />
-            <button className="update-password" onClick={handleUpdatePassword}>
-            Update
-            </button>
-        </div>
-        </div>
+            <Button color='secondary' variant='contained' onClick={handleUpdatePassword}>Update Password</Button>
+          </Grid>
+          <Grid xs={6}>
+            <Button color='secondary' variant='contained' onClick={handleUploadImage}>Upload Profile Picture</Button>
+            <Button color='secondary' variant='contained' onClick={handleDeleteAccount}>Delete Account</Button>
+            <Button color='secondary' variant='contained' onClick={handleBack}>Back</Button>
+          </Grid>
+      </Grid>
+    </Box>
   )
 }
 
