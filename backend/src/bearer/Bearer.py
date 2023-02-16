fee43699a88fca7420b71577c037d2e2d79a2d94
pandabear100 <nelsonysun@gmail.com>
2023-02-16 00:34:30 -0800

    Added ability to revoke token, and wrote a test suite

--- backend/src/bearer/Bearer.py
+++ backend/src/bearer/Bearer.py
@@ -11,6 +11,11 @@ class Bearer:
         self.bearer_dict = {}
 
     def add_token(self, value: str) -> str:
+        """
+        Adds a token to the bearer object to correspond to the identifying value.
+        :param value: a string identifying the client
+        :return: a random 512-bit number as a string
+        """
         token = secrets.token_hex(16)
         while token in self.bearer_dict.keys():  # guarantee no conflicts
             token = secrets.token_hex(16)
@@ -18,4 +23,19 @@ class Bearer:
         return token
 
     def verify_token(self, token: str, value: str) -> bool:
+        """
+        Verifies that a token belongs to a client, returning true if so, and false if not.
+        :param token: the authentication token the client provides for authentication
+        :param value: a string identifying the client
+        :return: true if the token exists and belongs to the client, and false otherwise
+        """
         return token in self.bearer_dict.keys() and self.bearer_dict[token] == value
+
+    def revoke_token(self, token: str):
+        """
+        Revokes a token. After use, the revoked token can no longer be used for authentication,
+        including by the client that originally owned the token.
+        :param token: the token to revoke.
+        """
+        if token in self.bearer_dict.keys():
+            self.bearer_dict.pop(token)
--- backend/tests/test_bearer.py
+++ backend/tests/test_bearer.py
@@ -1,1 +1,52 @@
-def
+from backend.src.bearer.Bearer import Bearer
+
+
+def test_add_one_token():
+    bearer = Bearer()
+    client = 'client1'
+    token = bearer.add_token(client)
+    assert bearer.verify_token(token, client)
+
+
+def test_one_client_multiple_tokens():
+    bearer = Bearer()
+    client = 'client1'
+    token1 = bearer.add_token(client)
+    token2 = bearer.add_token(client)
+    assert bearer.verify_token(token1, client)
+    assert bearer.verify_token(token2, client)
+
+
+def test_empty_string():
+    bearer = Bearer()
+    client = ''
+    token = bearer.add_token(client)
+    assert bearer.verify_token(token, client)
+
+
+def test_bad_token():
+    bearer = Bearer()
+    client = 'client1'
+    bearer.add_token(client)
+    assert not bearer.verify_token('bad_token', client)
+
+
+def test_multiple_clients():
+    bearer = Bearer()
+    client1 = 'client1'
+    client2 = 'client2'
+    token1 = bearer.add_token(client1)
+    token2 = bearer.add_token(client2)
+    assert bearer.verify_token(token1, client1)
+    assert bearer.verify_token(token2, client2)
+    assert not bearer.verify_token(token1, client2)
+    assert not bearer.verify_token(token2, client1)
+
+
+def test_revoke_token():
+    bearer = Bearer()
+    client = 'client1'
+    token = bearer.add_token(client)
+    assert bearer.verify_token(token, client)
+    bearer.revoke_token(token)
+    assert not bearer.verify_token(token, client)
