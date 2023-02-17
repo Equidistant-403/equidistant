d9fdbd03f8ea9ad55f2cc5533d104b8985ea3983
jspaniac <jspaniac@cs.washington.edu>
2023-02-16 22:13:56 -0800

    Cleaning up style

--- backend/src/api/nominatim.py
+++ backend/src/api/nominatim.py
@@ -3,20 +3,23 @@ from typing import Tuple
 
 nominatim_endpoint = "https://nominatim.openstreetmap.org"
 address_query = "/search/{0}?format=json"
+
+
 def get_lat_long(address: str) -> Tuple[float, float]:
-  response = requests.get(nominatim_endpoint + address_query.format(address))
+    response = requests.get(nominatim_endpoint + address_query.format(address))
 
-  if response.status_code != 200: 
-    # TODO: What's an appropriate return type if the request errors?
-    return None
+    if response.status_code != 200:
+        # TODO: What's an appropriate return type if the request errors?
+        return None
 
-  response_json = response.json()
-  if (len(response_json) == 0 or
-      "lat" not in response_json[0] or "lon" not in response_json[0]):
-    # TODO: What's an appropriate return type if the request errors?
-    return None
+    response_json = response.json()
+    if (len(response_json) == 0 or
+            "lat" not in response_json[0] or "lon" not in response_json[0]):
+        # TODO: What's an appropriate return type if the request errors?
+        return None
 
-  return (float(response_json[0]["lat"]), float(response_json[0]["lon"]))
+    return (float(response_json[0]["lat"]), float(response_json[0]["lon"]))
+
 
 if __name__ == "__main__":
-  print(get_lat_long('3800 E Stevens Way NE, Seattle, WA 98195'))
+    print(get_lat_long('3800 E Stevens Way NE, Seattle, WA 98195'))
--- backend/src/api/overpass.py
+++ backend/src/api/overpass.py
@@ -7,23 +7,26 @@ bounding_query = """
   node[amenity="restaurant"](around:{0}, {1},{2});
   out;
 """
+
+
 def nearby_point(lat_long: Tuple[float, float], radius: int) -> dict:
-  response = requests.get(overpass_endpoint, params={
-    'data': bounding_query.format(
-      radius, lat_long[0], lat_long[1]
-    )
-  })
+    response = requests.get(overpass_endpoint, params={
+        'data': bounding_query.format(
+            radius, lat_long[0], lat_long[1]
+        )
+    })
 
-  if response.status_code != 200:
-    # TODO: What's a useful response here
-    return None
+    if response.status_code != 200:
+        # TODO: What's a useful response here
+        return None
 
-  response_json = response.json()
-  if "elements" not in response_json:
-    # TODO: What's a useful response here
-    return None
+    response_json = response.json()
+    if "elements" not in response_json:
+        # TODO: What's a useful response here
+        return None
 
-  return response_json['elements']
+    return response_json['elements']
+
 
 if __name__ == "__main__":
-  print(nearby_point((47.6579925, -122.3133866), 1000))
+    print(nearby_point((47.6579925, -122.3133866), 1000))
