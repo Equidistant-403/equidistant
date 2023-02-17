de9fc067d7b9adc098a2e3570b45e88ff9a99a1e
Alex <adundarov@gmail.com>
2023-02-16 20:47:40 -0800

    worked more on results

--- frontend/my-app/src/Pages/Results.tsx
+++ frontend/my-app/src/Pages/Results.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Results.css'
 import { useLocation, useNavigate } from 'react-router-dom'
 import type { Location } from '../responseTypes'
-import { Box, Button, Typography } from '@mui/material'
+import { Button, Typography, Paper } from '@mui/material'
 import { Stack } from '@mui/system'
 
 const Results: React.FC = () => {
@@ -28,20 +28,85 @@ const Results: React.FC = () => {
   }
 
   return (
-        <Stack>
-        <Typography
-          variant="h3"
-          component="h2">
-            Results
-        </Typography>
-        <Box>
-          <Typography
-            variant="h5"
-            component="h2">
-              Current location: {result.name}
-          </Typography>
-        </Box>
-        <Stack
+    <Stack
+      direction="column"
+      height="100%"
+      alignItems="center"
+      justifyContent="space-evenly"
+      spacing={2}>
+      <Typography
+        variant="h3"
+        component="h2"
+        sx={{
+          py: 5,
+          mx: 'auto'
+        }}>
+          Results
+      </Typography>
+        <Stack // results
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={2}
+          sx={{
+            display: 'flex',
+            flexWarp: 'wrap',
+            backgroundColor: 'secondary.main',
+            mt: 10,
+            borderRadius: '40px',
+            width: '600px',
+            height: '600px'
+          }}>
+          <Stack>
+            <Typography // times
+              variant="h3"
+              component="h2"
+              sx={{
+                py: 5,
+                mx: 'auto'
+              }}>
+                Results
+            </Typography>
+          </Stack>
+          <Stack // card
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={2}
+            sx={{
+              display: 'flex',
+              flexWarp: 'wrap',
+              backgroundColor: 'primary.main',
+              mt: 10,
+              borderRadius: '40px',
+              width: '600px',
+              height: '600px'
+            }}>
+              <Paper
+                sx={{
+                  mt: 3,
+                  width: '550px',
+                  height: '380px',
+                  backgroundColor: 'primary.main',
+                  borderRadius: '10px'
+                }}>
+                <Paper
+                  sx={{
+                    mt: 2,
+                    mx: 2,
+                    p: 2,
+                    borderRadius: '10px'
+                  }}>
+                  <Typography
+                    variant="h5"
+                    component="h2"
+                    textAlign="center">
+                    {result.name}
+                  </Typography>
+                </Paper>
+              </Paper>
+          </Stack>
+        </Stack>
+      <Stack
         direction="row"
         justifyContent="flex-start"
         alignItems="center"
@@ -49,7 +114,7 @@ const Results: React.FC = () => {
         sx={{
           display: 'flex',
           flexWarp: 'wrap',
-          m: 'auto'
+          alignItems: ''
         }}>
         <Button
         color='secondary'
@@ -60,24 +125,22 @@ const Results: React.FC = () => {
           flexWarp: 'wrap',
           mr: 3,
           borderRadius: 28
-        }}
-        >
+        }}>
           Next
-      </Button>
-      <Button
-      color='secondary'
-      variant='contained'
-      onClick={handleDoneClick}
-      sx={{
-        display: 'flex',
-        flexWarp: 'wrap',
-        mr: 3,
-        borderRadius: 28
-      }}
-      >
-        Done
-    </Button>
-        </Stack>
+        </Button>
+        <Button
+          color='secondary'
+          variant='contained'
+          onClick={handleDoneClick}
+          sx={{
+            display: 'flex',
+            flexWarp: 'wrap',
+            mr: 3,
+            borderRadius: 28
+          }}>
+          Done
+        </Button>
+      </Stack>
     </Stack>
   )
 }
