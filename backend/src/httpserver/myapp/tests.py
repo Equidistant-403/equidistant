65ee5dfd1ab1400fcde9ce8c17b12162edea5fc1
pandabear100 <nelsonysun@gmail.com>
2023-02-19 23:13:15 -0800

    Added a single test, and commented views

--- backend/src/httpserver/myapp/tests.py
+++ backend/src/httpserver/myapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class AppTests(TestCase):
+
+    def test_receive_response(self):
+        """
+        Basic test case to determine if app responds to a request. Should be altered after
+        logic is updated.
+        """
+        response = self.client.get(reverse('user'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Thanks for submitting your data')
--- backend/src/httpserver/myapp/views.py
+++ backend/src/httpserver/myapp/views.py
@@ -2,37 +2,65 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
 
+
 def post_create_user(request):
+    """
+    Handles the /create endpoint.
+    """
     if request.method == 'POST':
         data = request.POST
     return HttpResponse('Thanks for submitting your data')
+
 
 def get_login_credentials(request):
+    """
+    Handles the /login endpoint.
+    """
     if request.method == 'GET':
         data = request.GET
     return HttpResponse('Thanks for submitting your data')
+
 
 def get_locations(request):
+    """
+    Handles the /locations endpoint.
+    """
     if request.method == 'GET':
         data = request.GET
     return HttpResponse('Thanks for submitting your data')
+
 
 def get_friends(request):
+    """
+    Handles the /friends endpoint.
+    """
     if request.method == 'GET':
         data = request.GET
     return HttpResponse('Thanks for submitting your data')
+
 
 def get_user(request):
+    """
+    Handles the /user endpoint.
+    """
     if request.method == 'GET':
         data = request.GET
     return HttpResponse('Thanks for submitting your data')
 
+
 def post_friend_request(request):
+    """
+    Handles the /sendFriendReq endpoint.
+    """
     if request.method == 'POST':
         data = request.POST
     return HttpResponse('Thanks for submitting your data')
 
+
 def post_friend_request_response(request):
+    """
+    Handles the /respondFriendReq endpoint.
+    """
     if request.method == 'POST':
         data = request.POST
     return HttpResponse('Thanks for submitting your data')
