502189ddeb059329c5b3a58627eb3640c8ff22e7
jspaniac <jspaniac@cs.washington.edu>
2023-02-20 16:55:28 -0800

    Allowing CORS

--- backend/src/httpserver/httpserver/settings.py
+++ backend/src/httpserver/httpserver/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'myapp'
+    'myapp',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "httpserver.urls"
@@ -116,3 +118,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True
+# For now, letting all CORS requests go through, in the future we can set this to False
+# and uncomment below to get a specific whitelist
+'''
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:*',
+)
+'''
