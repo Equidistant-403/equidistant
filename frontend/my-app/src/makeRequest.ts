2c531d9c543f2ec3087b656d8a11febabad345d1
jspaniac <jspaniac@cs.washington.edu>
2023-02-21 14:27:41 -0800

    Updating to obey API contract and converting snake to camel case

--- frontend/my-app/src/makeRequest.ts
+++ frontend/my-app/src/makeRequest.ts
@@ -3,6 +3,21 @@ import type { EquidistantResponse } from './responseTypes'
 
 export default async function makeRequest (request: EquidistantRequest): Promise<EquidistantResponse> {
   const response = await fetch(request.path, request)
-  const json = await response.json()
+  const json = JSON.parse(snakeCaseToCamelCase(await response.text()))
   return json
+}
+
+function snakeCaseToCamelCase (input: string): string {
+  input
+    .split('_')
+    .reduce(
+      (res, word, i) =>
+        i === 0
+          ? word.toLowerCase()
+          : `${res}${word.charAt(0).toUpperCase()}${word
+              .substr(1)
+              .toLowerCase()}`,
+      ''
+    )
+  return input
 }
