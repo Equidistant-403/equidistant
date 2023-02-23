1d415182f00852aadbf8874f02aafcdc442559ac
jspaniac <jspaniac@cs.washington.edu>
2023-02-22 22:53:12 -0800

    Refactoring requests for POST body

--- frontend/my-app/src/requestObjects.ts
+++ frontend/my-app/src/requestObjects.ts
@@ -7,13 +7,22 @@ const ENDPOINT = process.env.NODE_ENV === 'development' ? '' : process.env.REACT
 class EquidistantRequest {
   path: string
   method: HttpMethods
+  body: string
   headers: Headers
 
-  constructor (method: HttpMethods, path: string) {
+  constructor (method: HttpMethods, path: string, body: Record<string, string>) {
     this.path = ENDPOINT + path
     this.method = method
+    this.body = ''
+
+    if (method === HttpMethods.GET) {
+      this.path = this.addParams(new URLSearchParams(body))
+    } else if (method === HttpMethods.POST) {
+      this.body = JSON.stringify(body)
+    }
+
     this.headers = new Headers({
-      'Content-Type': 'text/plain',
+      'Content-type': 'application/json; charset=UTF-8',
       'ngrok-skip-browser-warning': 'true'
     })
   }
@@ -29,31 +38,29 @@ export { EquidistantRequest }
 // Login
 class LoginRequest extends EquidistantRequest {
   constructor (email: string, password: string) {
-    super(HttpMethods.GET, '/login')
-    this.path = super.addParams(new URLSearchParams({
+    super(HttpMethods.GET, '/login', {
       email,
       password
-    }))
+    })
   }
 }
 
 // Create
 class CreateAccountRequest extends EquidistantRequest {
   constructor (email: string, password: string, address: string) {
-    super(HttpMethods.POST, '/create')
-    this.path = super.addParams(new URLSearchParams({
+    super(HttpMethods.POST, '/create', {
       email,
       password,
       address
-    }))
+    })
   }
 }
 
 export { LoginRequest, CreateAccountRequest }
 
 class BearerRequest extends EquidistantRequest {
-  constructor (method: HttpMethods, path: string, bearer: string) {
-    super(method, path)
+  constructor (method: HttpMethods, path: string, bearer: string, body: Record<string, string>) {
+    super(method, path, body)
     this.headers.append('Authorization', bearer)
   }
 }
@@ -61,8 +68,7 @@ class BearerRequest extends EquidistantRequest {
 // Location
 class LocationRequest extends BearerRequest {
   constructor (users: string[], bearer: string) {
-    super(HttpMethods.GET, '/locations', bearer)
-
+    super(HttpMethods.GET, '/locations', bearer, {})
     const params = new URLSearchParams()
     users.forEach((user) => {
       params.append('users', user)
@@ -74,42 +80,38 @@ class LocationRequest extends BearerRequest {
 // Friends
 class FriendsRequest extends BearerRequest {
   constructor (email: string, bearer: string) {
-    super(HttpMethods.GET, '/friends', bearer)
-    this.path = super.addParams(new URLSearchParams({
+    super(HttpMethods.GET, '/friends', bearer, {
       email
-    }))
+    })
   }
 }
 
 // User
 class UserRequest extends BearerRequest {
   constructor (email: string, bearer: string) {
-    super(HttpMethods.GET, '/user', bearer)
-    this.path = super.addParams(new URLSearchParams({
+    super(HttpMethods.GET, '/user', bearer, {
       email
-    }))
+    })
   }
 }
 
 // Friend request
 class SendFriendRequest extends BearerRequest {
   constructor (requesterEmail: string, receiverEmail: string, bearer: string) {
-    super(HttpMethods.POST, '/sendFriendReq', bearer)
-    this.path = super.addParams(new URLSearchParams({
+    super(HttpMethods.POST, '/sendFriendReq', bearer, {
       requesterEmail,
       receiverEmail
-    }))
+    })
   }
 }
 
 // Friend request response
 class FriendRequestResponse extends BearerRequest {
   constructor (receiverEmail: string, requesterEmail: string, bearer: string) {
-    super(HttpMethods.POST, '/respondFriendReq', bearer)
-    this.path = super.addParams(new URLSearchParams({
+    super(HttpMethods.POST, '/respondFriendReq', bearer, {
       receiverEmail,
       requesterEmail
-    }))
+    })
   }
 }
 
