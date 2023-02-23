391c3ec6f564175284197cdee9a415c4a2405089
zshafe <zshafe@users.noreply.github.com>
2023-02-23 23:46:39 +0000

    deploy: 540d69d643f6a7f062fca3e0369c520e771300bf

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import Account from './Pages/Account'
+import CreateAccount from './Pages/CreateAccount'
+import LandingPage from './Pages/LandingPage'
+import LoginPage from './Pages/LoginPage'
+import Results from './Pages/Results'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: { // grey
+      main: '#dedede'
+    },
+    secondary: { // blue
+      main: '#1a7fc1'
+    },
+    background: { // background
+      default: '#202020', // grey
+      paper: '#1a7fc1' // blue
+    },
+    text: { // text
+      primary: 'rgba(255,255,255,0.87)',
+      secondary: 'rgba(0,0,0,0.6)'
+    },
+    success: { // green
+      main: '#3b7461'
+    }
+  }
+})
+
+function App (): React.ReactElement {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/create-account" element={<CreateAccount/>} />
+        <Route path="/landing" element={<LandingPage/>} />
+        <Route path="/results" element={<Results/>} />
+        <Route path="/account" element={<Account/>} />
+      </Routes>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,320 @@
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { isError } from '../responseTypes'
+import { LocationRequest } from '../requestObjects'
+import makeRequest from '../makeRequest'
+import {
+  AppBar,
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Paper,
+  Toolbar,
+  IconButton,
+  Button,
+  Stack,
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField
+} from '@mui/material'
+import type { LocationResponse, User } from '../responseTypes'
+
+const LandingPage: React.FC = () => {
+  const [open, setOpen] = useState(false)
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const [friends] = useState<User[]>(location.state.friends)
+  // TODO: Allow user to accept friend requests
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [requests, setRequests] = useState<User[]>(location.state.requests)
+  const user = location.state.user
+  const bearer = location.state.bearer
+
+  const [checkedFriends, setCheckedFriends] = useState(() => friends.map((i) => false))
+
+  const toggleCheckbox = (index: number, checked: any): void => {
+    setCheckedFriends((isChecked) => {
+      return isChecked.map((c, i) => {
+        if (i === index) return checked
+        return c
+      })
+    })
+  }
+
+  const getCheckedFriends = (): User[] => {
+    const users: User[] = friends.filter((friend, index) => checkedFriends[index])
+    return users
+  }
+
+  const handleGenerateClick = (): void => {
+    makeRequest(new LocationRequest([user.email, ...getCheckedFriends()], bearer))
+      .then((res) => {
+        if (isError(res)) {
+          // TODO: Display this error message
+          // TODO: remove console.log
+          console.log(res.errorMessage)
+          return
+        }
+
+        const response = (res as LocationResponse)
+        navigate('/results', {
+          state: {
+            locations: response.locations
+          }
+        })
+      })
+      .catch((e) => { console.error(e) })
+  }
+
+  const handlAccountClick = (): void => {
+    navigate('/account', {
+      state: {
+        email: user.email,
+        location: user.address
+      }
+    })
+  }
+
+  const handleLogoutClick = (): void => {
+    console.log('dummy logout actuation')
+    navigate('/')
+  }
+
+  const handleFriendMenu = (): void => {
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      sx={{
+        borderRadius: '20px'
+      }}>
+      <DialogTitle>Add Friend</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Send Request</Button>
+      </DialogActions>
+    </Dialog>
+  }
+
+  const handleToggle = (): void => {
+    setOpen(true)
+  }
+
+  const handleSubmit = (): void => {
+    console.log('add friend')
+    handleClose()
+  }
+
+  const handleClose = (): void => {
+    setOpen(false)
+  }
+
+  return (
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '16px'
+        }}>
+          <AppBar
+            position='relative'
+            sx={{
+              color: '1a7fc1',
+              alignSelf: 'start',
+              width: 'auto',
+              minWidth: 4 / 5,
+              mb: 'auto',
+              mt: 5,
+              mx: 'auto',
+              borderRadius: '40px'
+            }}
+          >
+            <Toolbar>
+              <Button color='secondary' variant='contained' sx={{ borderRadius: 28 }}onClick={handleLogoutClick}>Logout</Button>
+                <Stack
+                  direction="row"
+                  justifyContent="flex-start"
+                  alignItems="center"
+                  spacing={2}
+                  sx={{
+                    display: 'flex',
+                    flexWarp: 'wrap',
+                    m: 'auto'
+                  }}
+                >
+                  {getCheckedFriends().map((user, index) => (
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                      sx={{
+                        width: 50,
+                        height: 50,
+                        backgroundColor: 'primary.dark',
+                        '&:hover': {
+                          backgroundColor: 'success.main',
+                          opacity: [0.8, 0.8, 0.8]
+                        }
+                      }}
+                      key={index}
+                      onClick={handleFriendMenu}
+                    >
+                      <Typography
+                        variant="h5"
+                        component="h2">
+                        {user.email[0]}
+                      </Typography>
+                    </IconButton>
+                  ))}
+                </Stack>
+              <Button
+                color='secondary'
+                variant='contained'
+                onClick={handleGenerateClick}
+                sx={{
+                  display: 'flex',
+                  flexWarp: 'wrap',
+                  mr: 3,
+                  borderRadius: 28
+                }}
+                >
+                  Generate
+              </Button>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="label"
+                sx={{
+                  width: 50,
+                  height: 50,
+                  backgroundColor: 'primary.dark',
+                  '&:hover': {
+                    backgroundColor: 'success.main',
+                    opacity: [0.8, 0.8, 0.8]
+                  }
+                }}
+                onClick={handlAccountClick}
+              >
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Box
+            component="span"
+            sx={{ p: 5 }}
+          />
+        <Button variant="outlined" color="primary" onClick={handleToggle} sx={{ mb: 5 }}>
+            Add Friends
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          sx={{
+            borderRadius: '20px'
+          }}>
+          <DialogTitle>Add Friend</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleSubmit}>Send Request</Button>
+          </DialogActions>
+        </Dialog>
+        <Paper
+          sx={{
+            minWidth: 1 / 3,
+            maxHeight: 1 / 2,
+            overflow: 'auto',
+            mb: 'auto',
+            borderRadius: '40px'
+          }}
+        >
+          <FormGroup
+            sx={{
+              backgroundColor: 'primary.main',
+              py: 1
+            }}
+          >
+            {friends.map((friend, index) => (
+              <FormControlLabel
+                sx={{
+                  backgroundColor: 'secondary.main',
+                  ml: 2,
+                  my: 0.3,
+                  py: 1,
+                  borderRadius: '20px',
+                  '&:hover': {
+                    backgroundColor: 'secondary.main',
+                    opacity: [0.8, 0.8, 0.8]
+                  }
+                }}
+                key={friend.email}
+                control={
+                  <Checkbox
+                    checked={checkedFriends[index]}
+                    onChange={(e) => { toggleCheckbox(index, e.target.checked) } }
+                  />}
+                  label={
+                  <Stack
+                    direction="row"
+                    alignItems="center"
+                    spacing={2}
+                  >
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                      sx={{
+                        width: 50,
+                        height: 50,
+                        backgroundColor: 'primary.dark',
+                        '&:hover': {
+                          backgroundColor: 'success.main',
+                          opacity: [0.6, 0.6, 0.6]
+                        }
+                      }}
+                      key={index}
+                      onClick={handleFriendMenu}
+                    >
+                      <Typography variant="h5" component="h2">
+                        {friend.email[0]}
+                      </Typography>
+                    </IconButton>
+                    <Typography variant="h5" component="h2">
+                      {friend.email}
+                    </Typography>
+                  </Stack>
+                }/>
+            ))}
+          </FormGroup>
+        </Paper>
+      </Box>
+  )
+}
+
+export default LandingPage
