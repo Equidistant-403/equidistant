b8141fb3a5b7a0fee57eac374663c05e06a95888
zshafe <zshafe@users.noreply.github.com>
2023-02-23 23:50:13 +0000

    deploy: 4dc2b19494d223ba70633bb8f2d6f2d139ffbc08

--- src/makeRequest.ts
+++
@@ -1,8 +0,0 @@
-import type { EquidistantRequest } from './requestObjects'
-import type { EquidistantResponse } from './responseTypes'
-
-export default async function makeRequest (request: EquidistantRequest): Promise<EquidistantResponse> {
-  const response = await fetch(request.path, request)
-  const json = await response.json()
-  return json
-}
