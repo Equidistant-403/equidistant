6810126ac3edb17a89d23e98900ad70acf98f3b8
zshafe <zshafe@cs.washington.edu>
2023-02-24 13:42:39 -0800

    removed comments and finalized action

--- frontend/my-app/src/index.tsx
+++ frontend/my-app/src/index.tsx
@@ -32,27 +32,6 @@ async function main () {
 
 void main()
 
-// if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MOCKS_ENABLED === 'true') {
-//   // eslint-disable-next-line @typescript-eslint/no-var-requires
-//   const { worker } = require('./mocks/browser')
-//   worker.start({
-//     serviceWorker: {
-//       url: '/equidistant/mockServiceWorker.js'
-//     }
-//   })
-// }
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// )
-// root.render(
-//   <React.StrictMode>
-//     <HashRouter basename='/'>
-//       <App />
-//     </HashRouter>
-//   </React.StrictMode>
-// )
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
